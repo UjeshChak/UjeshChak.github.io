841a8f82d394a5da41599bdfdca196568803b51e
UjeshChak <ujesh.chakravarty@jpschools.org>
2024-06-07 16:23:36 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,21 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle;
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas, 10, 10);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
+        for (var i = 0; i < 100; i++) {
+            drawCircle();
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,11 +47,14 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
-            
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i]);
+            }
+            // they were deleted because you need to do this 100 times for the 100 circles
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
-
+            for (var i = 0; i < circles.length; i++) {
+                game.checkCirclePosition(circles[i]);
+            }
             // TODO 9 : Iterate over the array
            
             
@@ -55,14 +66,22 @@ var init = function (window) {
         it to the opposite side of the screen.
         */
         game.checkCirclePosition = function(circle) {
-
+            var rightEdge = circle.x + circle.radius;
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
+            if ( rightEdge > canvas.width ) {
+                circle.x = circle.radius;
             }
-            
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (rightEdge < 0) {
+                circle.x = canvas.width - circle.radius;
+            }
+            var topEdge = circle.y + circle.radius;
+            if (topEdge < 0) {
+                circle.y = canvas.height - circle.radius;
+            }
+            if (topEdge > canvas.height) {
+                circle.y = circle.radius;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
